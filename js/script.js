f087f1bf18a97a1655491736093b0e82633a2c49
Rahad-khan <clash146098@gmail.com>
2022-02-17 17:39:47 +0600

    more error handeling added

--- js/script.js
+++ js/script.js
@@ -8,31 +8,39 @@ function takeInputValue(inputid) {
 Error function
 ================== */
 // clear error messages function
-function errorNone() {
+function errorNone(clearValue) {
   const errors = document.getElementsByClassName("errors");
   for (const error of errors) {
     error.style.display = "none";
   }
+  if (clearValue) {
+    document.getElementById("total-expense").innerText = "00";
+    document.getElementById("remaining-amount").innerText = "00";
+  } else if(clearValue== false) {
+    document.getElementById("total-saving").innerText = "00";
+    document.getElementById("remaining-balance").innerText = "00";
+  }
 }
+document.getElementById("total-expense").innerText = "00";
 // isNaN error function
-function notANumber(value1, value2, inputid) {
+function notANumber(value1, value2, inputid,validation) {
   if (isNaN(value1) || isNaN(value2)) {
-    errorNone();
+    errorNone(validation);
     document.getElementById("error-" + inputid).style.display = "block";
     return true;
   }
 }
 // negative value error function
-function isNegative(idString) {
+function isNegative(idString,validation) {
   for (const argument of arguments) {
     if (argument < 0) {
-      errorNone();
+      errorNone(validation);
       document.getElementById("error-" + idString).style.display = "block";
       return true;
     }
   }
 }
-//calculate button click 
+//calculate button click
 document
   .getElementById("calculate-total")
   .addEventListener("click", function () {
@@ -42,7 +50,7 @@ document
     const clothAmount = takeInputValue("cloth");
     const totalExpense = foodAmount + rentAmount + clothAmount;
     // isNotANumber handeling
-    const numberCheck = notANumber(totalExpense, incomeAmount, "number");
+    const numberCheck = notANumber(totalExpense, incomeAmount, "number",true);
     debugger;
     if (numberCheck == true) {
       return;
@@ -50,6 +58,7 @@ document
     // negativeNumber handeling
     const negativeNumber = isNegative(
       "negative",
+      true,
       incomeAmount,
       foodAmount,
       rentAmount,
@@ -69,30 +78,34 @@ document
       incomeAmount - totalExpense;
     errorNone();
   });
-  //save button click 
+//save button click
 document.getElementById("savings").addEventListener("click", function () {
   const savingInput = takeInputValue("saving");
   const incomeAmount = takeInputValue("income");
   const totalSaving = incomeAmount * (savingInput / 100);
   //   errors
   // check NaN errors
-  const numberCheck = notANumber(savingInput, incomeAmount, "validation");
+  const numberCheck = notANumber(savingInput, incomeAmount, "validation",false);
   if (numberCheck == true) {
     return;
   }
   // check Negative value errors
-  const negativeNumber = isNegative("negative-2nd",savingInput,incomeAmount);
-  if (negativeNumber==true) {
+  const negativeNumber = isNegative("negative-2nd",false, savingInput, incomeAmount);
+  if (negativeNumber == true) {
     return;
   }
-  document.getElementById("total-saving").innerText = totalSaving;
+  errorNone();
+  document.getElementById("total-saving").innerText = (totalSaving).toFixed(2);
   const balanceText = document.getElementById("remaining-amount").innerText;
   const balance = parseFloat(balanceText);
   // saving is more than remaining balance type error
   if (balance < totalSaving) {
-    errorNone();
+    // errorNone();
+    document.getElementById("remaining-balance").innerText = "00";
     return (document.getElementById("error-saving").style.display = "block");
+  } else {
+    document.getElementById("error-saving").style.display = "none"
+    document.getElementById("remaining-balance").innerText =
+      (balance - totalSaving).toFixed(2);
   }
-  document.getElementById("remaining-balance").innerText =
-    balance - totalSaving;
 });
